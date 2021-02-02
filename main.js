cb76db3e88f6af6f058c1164faf545dcea21dc99
y-solb <ysolb99@gmail.com>
2021-02-02 23:16:23 +0900

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -11,3 +11,15 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("navbar--dark");
   }
 });
+
+//Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+});
