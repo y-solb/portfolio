d6d9c5f8593739e4a04d1dda05ce91e6c17ea9df
y-solb <ysolb99@gmail.com>
2021-02-03 22:51:24 +0900

    JS: Arrow up

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ navbarMenu.addEventListener("click", (event) => {
   scrollIntoView(link);
 });
 
-//Handle click on "contact me" button on home
+//Handle click on 'contact me' button on home
 const homeContactBtn = document.querySelector(".home__contact");
 homeContactBtn.addEventListener("click", (event) => {
   scrollIntoView("#contact");
@@ -35,6 +35,21 @@ const homeHeight = home.getBoundingClientRect().height;
 
 document.addEventListener("scroll", () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector(".arrow__up");
+document.addEventListener("scroll", () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add("visible");
+  } else {
+    arrowUp.classList.remove("visible");
+  }
+});
+
+// Handle click on the "arrow up" button
+arrowUp.addEventListener("click", () => {
+  scrollIntoView("#home");
 });
 
 function scrollIntoView(selector) {
