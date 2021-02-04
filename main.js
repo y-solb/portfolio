a9e25b8ea34e3dc1453205cb9729fc088d4f32c7
y-solb <ysolb99@gmail.com>
2021-02-04 23:29:21 +0900

    JS: navbar toggle btn for small screen

--- main.js
+++ main.js
@@ -20,7 +20,14 @@ navbarMenu.addEventListener("click", (e) => {
   if (link == null) {
     return;
   }
+  navbarMenu.classList.remove("open");
   scrollIntoView(link);
+});
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
+navbarToggleBtn.addEventListener("click", () => {
+  navbarMenu.classList.toggle("open");
 });
 
 //Handle click on 'contact me' button on home
